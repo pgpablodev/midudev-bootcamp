7dc3dc8bf52b3279988a67d04c67877a383f3d2c
pgpablodev <povarg.pablo@gmail.com>
2022-06-06 20:32:20 +0200

    Second commit

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,27 +1,51 @@
-const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-  
-    return (
+const Titulo = (props) => {    
+    return(
+        <h1>{props.course}</h1>
+    );
+}
+
+const Parte = (props) => {
+    return(
+        <div>
+            <p>
+            {props.part.name} {props.part.exercises}
+            </p>
+        </div>
+    );
+}
+
+const Contenido = (props) => {
+    return(
+        <div>
+            <Parte part={props.parts[0]}/>
+            <Parte part={props.parts[1]}/>
+            <Parte part={props.parts[2]}/>
+        </div>
+    );
+}
+
+const Total = (props) => {
+    return(
+        <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    );
+}
+
+const App = () => {    
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {name: 'Fundamentals of React', exercises: 10},
+            {name: 'Using props to pass data', exercises: 7},
+            {name: 'State of a component', exercises: 14}
+        ]
+    };
+    return(
       <div>
-        <h1>{course}</h1>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+          <Titulo course={course.name}/>
+          <Contenido parts={course.parts}/>
+          <Total parts={course.parts}/>
       </div>
-    )
+    );
   }
-  
-  export default App
+
+  export default App;
