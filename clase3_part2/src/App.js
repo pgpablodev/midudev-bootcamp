a541080d73275a208db112c1c464997f4744bdeb
pgpablodev <povarg.pablo@gmail.com>
2022-06-08 23:20:15 +0200

    clase 3 terminada

--- clase3_part2/src/App.js
+++ clase3_part2/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+const WarningNotUsed = () => {
+  return <p>El componente todavía no se ha utilizado. </p>
+};
+
+const ListOfClicks = ({clicks}) => {
+  return <p>{clicks.join(", ")}</p>
+}
 
 function App() {
+  const [clicks, setClicks] = useState([]);
+
+  const handleClickLeft = () => {
+    setClicks((prevClicks) => [...prevClicks,'L']);
+  };
+
+  const handleClickRight = () => {
+    setClicks((prevClicks) => [...prevClicks,'R']);
+  };
+
+  const handleReset = () => {
+    setClicks([]);
+  }
+
+  const left = clicks.filter(click => click === 'L');
+  const right = clicks.filter(click => click === 'R');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {left.length}
+      <button onClick={handleClickLeft}>left</button>
+      <button onClick={handleClickRight}>right</button>
+      {right.length}
+      <p>
+        <button onClick={handleReset}>reset</button>
+      </p>
+      <p>Clicks totales: {clicks.length}</p>
+      {
+        clicks.length===0
+        ? <WarningNotUsed />
+        : <ListOfClicks clicks={clicks} /> //ODIO el operador ternario pero qué remedio...
+      }
     </div>
   );
 }
